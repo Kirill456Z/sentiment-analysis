30493c2356a457ed75ca2b2c979cd362999634d1
Kirill456Z <Kirill456Z@yandex.ru>
2022-03-29 20:46:25 +0300

    model modified

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def get_prediction(text=""):
     if request.method == "POST":
         text = request.form['text']
     prediction_prob = model.predict_proba([text])[0][1]
-    return render_template('index.html', text=text, result=proba_to_res(prediction_prob),
+    return render_template('index.html', text=text, result=proba_to_res(prediction_prob, model.best_threshold),
                            prediction_prob=f'{prediction_prob:.2f}')
 
 
--- util.py
+++ util.py
@@ -1,11 +1,11 @@
-def proba_to_res(proba):
-    if proba < 0.2:
+def proba_to_res(proba, best_threshold):
+    if proba < 0.4 * best_threshold:
         return "Отрицательный"
-    elif proba < 0.4:
+    elif proba < 0.8 * best_threshold:
         return "Вероятно отрицательный"
-    elif proba < 0.6:
+    elif proba < 1.2 * best_threshold:
         return "Нейтральный"
-    elif proba < 0.8:
+    elif proba < 1.6 * best_threshold:
         return "Вероятно положительный"
     else:
         return "Положительный"
